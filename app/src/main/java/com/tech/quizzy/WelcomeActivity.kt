f53472b68c2cd190390a2c3d6a14d1cc4d61b9cd
kaushal5504 <kaushalpatel0504@gmail.com>
2023-04-05 00:57:58 +0530

    spalsh screen added

--- app/src/main/java/com/tech/quizzy/WelcomeActivity.kt
+++ app/src/main/java/com/tech/quizzy/WelcomeActivity.kt
@@ -1,11 +1,40 @@
 package com.tech.quizzy
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Looper
+import android.view.animation.AnimationUtils
+import com.tech.quizzy.databinding.ActivityWelcomeBinding
+import java.util.logging.Handler
 
 class WelcomeActivity : AppCompatActivity() {
+    lateinit var splashBinding: ActivityWelcomeBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_welcome)
+        splashBinding = ActivityWelcomeBinding.inflate(layoutInflater)
+        setContentView(splashBinding.root)
+
+        val alphaAnimation = AnimationUtils.loadAnimation(applicationContext,R.anim.spalsh_anim)
+        splashBinding.textViewSplash.startAnimation(alphaAnimation)
+
+        //val handler = Handler(Looper.getMainLooper())
+
+        android.os.Handler().postDelayed(object :Runnable{
+            override fun run() {
+                intent = Intent(this@WelcomeActivity,MainActivity::class.java)
+                startActivity(intent)
+                finish()
+            }
+
+        },4000)
+
+
     }
 }
+
+
+
+
+
