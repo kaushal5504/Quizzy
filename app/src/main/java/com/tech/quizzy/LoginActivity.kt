eef741f36c8d23cc7b8de04e7b670a77d362273f
kaushal5504 <kaushalpatel0504@gmail.com>
2023-04-07 03:44:44 +0530

    added google sign in credentials

--- app/src/main/java/com/tech/quizzy/LoginActivity.kt
+++ app/src/main/java/com/tech/quizzy/LoginActivity.kt
@@ -1,20 +1,47 @@
 package com.tech.quizzy
 
 import android.content.Intent
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 import android.widget.Toast
+import androidx.activity.result.ActivityResultCallback
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.view.get
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
+import com.google.android.gms.tasks.Task
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
 import com.google.firebase.ktx.Firebase
 import com.tech.quizzy.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
     val auth = FirebaseAuth.getInstance()
     lateinit var loginBinding: ActivityLoginBinding
+    lateinit var googleSignInClient :GoogleSignInClient
+
+    lateinit var activityResultLauncher: ActivityResultLauncher<Intent>
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         loginBinding=ActivityLoginBinding.inflate(layoutInflater)
         setContentView(loginBinding.root)
+
+        //register activity result launcher in this method
+
+        registerActivityForGoogleSignin()
+
+
+        val textOfGoogleButton = loginBinding.buttonSigninGoogle.getChildAt(0)as TextView
+        textOfGoogleButton.text ="Contiue With Google"
+        textOfGoogleButton.setTextColor(Color.BLACK)
+        textOfGoogleButton.textSize = 18F
+
 
 
         loginBinding.buttonSignin.setOnClickListener {
@@ -25,7 +52,7 @@ class LoginActivity : AppCompatActivity() {
 
         }
         loginBinding.buttonSigninGoogle.setOnClickListener {
-
+            signInGoogle()
         }
 
         loginBinding.textViewSignUp.setOnClickListener {
@@ -62,6 +89,81 @@ class LoginActivity : AppCompatActivity() {
             }
 
         }
+
+
+    }
+
+    private fun signInGoogle()
+    {
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken("564981114398-6fadr9vi1h6dg14cq26st5l34ies7pu6.apps.googleusercontent.com")
+            .requestEmail().build()
+        googleSignInClient = GoogleSignIn.getClient(this,gso)
+
+
+        signIn()
+    }
+
+    private fun signIn()
+    {
+        val intent :Intent = googleSignInClient.signInIntent
+        activityResultLauncher.launch(intent)
+
+
+    }
+
+    private  fun registerActivityForGoogleSignin()
+    {
+
+        activityResultLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult(),
+            ActivityResultCallback {result->
+
+                val resultCode = result.resultCode
+                val data = result.data
+
+                if(resultCode == RESULT_OK && data !=null){
+
+                    val task : Task<GoogleSignInAccount> = GoogleSignIn.getSignedInAccountFromIntent(data)
+                    
+                    firebaseSignInWithGoogle(task)
+                }
+
+            })
+
+    }
+
+    private fun firebaseSignInWithGoogle(task: Task<GoogleSignInAccount>) {
+
+        try {
+            val account: GoogleSignInAccount = task.getResult(ApiException::class.java)
+            Toast.makeText(applicationContext,"Welcome to Quizzyy",Toast.LENGTH_SHORT).show()
+            val intent =Intent(this@LoginActivity,MainActivity::class.java)
+            startActivity(intent)
+            finish()
+
+            firebaseGoogleAccount(account)
+        }catch (e:Exception){
+            Toast.makeText(applicationContext,e.localizedMessage,Toast.LENGTH_SHORT).show()
+
+        }
+
+
+    }
+
+    private fun firebaseGoogleAccount(account: GoogleSignInAccount) {
+
+        val authCredential = GoogleAuthProvider.getCredential(account.idToken,null)
+        auth.signInWithCredential(authCredential).addOnCompleteListener { task->
+
+            if(task.isSuccessful){
+//                val user = auth.currentUser
+//                user.
+
+            }else{
+
+            }
+        }
+
     }
 
     override fun onStart() {
